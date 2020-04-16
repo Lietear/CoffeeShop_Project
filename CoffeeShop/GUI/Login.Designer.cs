848b993f453f29775d2f41891e689a9230d1b305
Lietear <akarapon1999@hotmail.com>
2020-04-16 21:16:26 +0700

    สร้างหน้าล็อกอิน(Prototype) ทดสอบการเชื่อมต่อกับ Database (เชื่อมต่อได้,ดึงข้อมูลใน Database มาเก็บในตัวแปรได้)

---
+++ CoffeeShop/GUI/Login.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace CoffeeShop.GUI
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.username = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.Label();
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.passTextBox = new System.Windows.Forms.TextBox();
+            this.Login_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.Location = new System.Drawing.Point(48, 65);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(55, 13);
+            this.username.TabIndex = 0;
+            this.username.Text = "Username";
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Location = new System.Drawing.Point(48, 103);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(53, 13);
+            this.password.TabIndex = 1;
+            this.password.Text = "Password";
+            // 
+            // userTextBox
+            // 
+            this.userTextBox.Location = new System.Drawing.Point(107, 65);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(195, 20);
+            this.userTextBox.TabIndex = 2;
+            // 
+            // passTextBox
+            // 
+            this.passTextBox.Location = new System.Drawing.Point(107, 100);
+            this.passTextBox.Name = "passTextBox";
+            this.passTextBox.Size = new System.Drawing.Size(195, 20);
+            this.passTextBox.TabIndex = 3;
+            // 
+            // Login_button
+            // 
+            this.Login_button.Location = new System.Drawing.Point(159, 140);
+            this.Login_button.Name = "Login_button";
+            this.Login_button.Size = new System.Drawing.Size(84, 23);
+            this.Login_button.TabIndex = 4;
+            this.Login_button.Text = "Login";
+            this.Login_button.UseVisualStyleBackColor = true;
+            this.Login_button.Click += new System.EventHandler(this.Login_button_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Login_button);
+            this.Controls.Add(this.passTextBox);
+            this.Controls.Add(this.userTextBox);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.TextBox passTextBox;
+        private System.Windows.Forms.Button Login_button;
+    }
+}
---
+++ CoffeeShop/GUI/Login.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CoffeeShop.GUI
+{
+    public partial class Login : Form
+    {
+        private String StaffCode;
+        private String StaffName;
+        private String Gender;
+        private String StaffLevel;
+        // ประกาศตัวแปรไว้สำหรับเก็บค่า
+
+        private string conn = "server = localhost; user id = root; password = 1234567890; persistsecurityinfo=True;database=coffee;allowuservariables=True";
+        //ประกาศตัวแปรเพื่อ connect ไว้สำหรับเชื่อมต่อกับ database
+        int i;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void getTheName(String StaffID) // ดึงข้อมูล StaffCode,StaffName,Gender,StaffLevel จาก Database ด้วย userTextBox
+        {
+            MySqlConnection connection = new MySqlConnection(conn);
+            connection.ConnectionString = conn;
+            String query = "SELECT StaffCode,StaffName,Gender,StaffLevel FROM staffs WHERE StaffID = '" + userTextBox.Text + "'";
+            try
+            {
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@username", StaffID);
+                cmd.ExecuteScalar();
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.Read())
+                {
+                    this.StaffCode = rdr["StaffCode"].ToString();
+                    this.StaffName = rdr["StaffName"].ToString();
+                    this.Gender = rdr["Gender"].ToString();
+                    this.StaffLevel = rdr["StaffLevel"].ToString();
+
+                }
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void connect(String username, String password) //เชื่อมต่อกับฐานข้อมูลเพื่อตราวสอบว่าพบข้อมูลใน Database หรือไม่
+        {
+            i = 0;
+            MySqlConnection connection = new MySqlConnection(conn);
+            connection.Open();
+            MySqlCommand cmd = connection.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from staffs where StaffID ='" + username + "' and StaffPassword ='" + password + "'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+
+            i = Convert.ToInt32(dt.Rows.Count.ToString());
+            if (i == 0) //ไม่พบข้อมูลใน Database
+            {
+                MessageBox.Show("you have entered invalid ID or Password");
+            }
+
+            else //พบข้อมูลใน Database
+            {
+                this.getTheName(userTextBox.Text);
+                this.Hide();
+                string Code = this.StaffCode.Trim();
+                string Name = this.StaffName.Trim();
+                string Gen = this.Gender.Trim();
+                string Level = this.StaffLevel.Trim();
+
+                //ทำหน้า Mainmenu ของ Staffs แต่ละระดับ **ยังคิดไม่ออก
+            }
+
+            connection.Close();
+        }
+
+        private void Login_button_Click(object sender, EventArgs e)
+        {
+            this.connect(userTextBox.Text, passTextBox.Text); //เรียกใช้ method connect เมื่อกดปุ่ม
+        }
+    }
+}
