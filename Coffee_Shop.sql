d6691e5dd0a01d935580a4bfd26275a6e3cc884e
Lietear <52730443+Lietear@users.noreply.github.com>
2020-05-03 15:13:36 +0700

    edit SQL

--- Coffee_Shop.sql
+++ Coffee_Shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 02, 2020 at 02:29 AM
+-- Generation Time: May 03, 2020 at 08:12 AM
 -- Server version: 8.0.17
 -- PHP Version: 7.3.10
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `final`
+-- Database: `coffee`
 --
 
 -- --------------------------------------------------------
@@ -31,11 +31,38 @@ SET time_zone = "+00:00";
 CREATE TABLE `customers` (
   `CustomerID` int(11) NOT NULL,
   `CustomerName` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `Gender` enum('M','F') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `Gender` enum('Male','Female') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `CustomerType` enum('Member','VIP','Other') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `CustomerTelNo` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `Points` int(11) NOT NULL
+  `Points` int(13) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`CustomerID`, `CustomerName`, `Gender`, `CustomerType`, `CustomerTelNo`, `Points`) VALUES
+(1, 'Akarapon Koedcharoenphon', 'Male', 'VIP', '0987654321', 950);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `discountrate`
+--
+
+CREATE TABLE `discountrate` (
+  `ID` int(11) NOT NULL,
+  `Rate` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `discountrate`
+--
+
+INSERT INTO `discountrate` (`ID`, `Rate`) VALUES
+(1, 10),
+(2, 50),
+(3, 100);
 
 -- --------------------------------------------------------
 
@@ -50,6 +77,16 @@ CREATE TABLE `products` (
   `ProductDetail` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`ProductID`, `ProductName`, `Price`, `ProductDetail`) VALUES
+(7, 'Product Name1', 50, 'Detail'),
+(8, 'Product Name2', 25, 'Detail'),
+(9, 'Product Name3', 30, 'Detail'),
+(10, 'Product Name4', 35, 'Detail');
+
 -- --------------------------------------------------------
 
 --
@@ -63,6 +100,13 @@ CREATE TABLE `sales` (
   `StaffID` int(11) NOT NULL,
   `GrandTotal` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`SaleID`, `SaleDateTime`, `CustomerID`, `StaffID`, `GrandTotal`) VALUES
+(15, '2/5/2563 13:32:23', 1, 6936938, 315);
 
 -- --------------------------------------------------------
 
@@ -79,6 +123,15 @@ CREATE TABLE `sale_details` (
   `Amount` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `sale_details`
+--
+
+INSERT INTO `sale_details` (`SaleDetailID`, `SaleID`, `ProductID`, `Price`, `Quantity`, `Amount`) VALUES
+(28, 15, 7, 50, 2, 100),
+(29, 15, 8, 25, 5, 125),
+(30, 15, 9, 30, 3, 90);
+
 -- --------------------------------------------------------
 
 --
@@ -89,10 +142,19 @@ CREATE TABLE `staffs` (
   `StaffID` int(11) NOT NULL,
   `StaffCode` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `StaffName` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `Gender` enum('M','F') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `Gender` enum('Male','Female') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `StaffPassword` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `StaffLevel` enum('Staff','Manager','Admin') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `staffs`
+--
+
+INSERT INTO `staffs` (`StaffID`, `StaffCode`, `StaffName`, `Gender`, `StaffPassword`, `StaffLevel`) VALUES
+(1, 'M88643', 'Akarapon Koedcharoenphon', 'Male', '1', 'Manager'),
+(2, 'S85023', 'Akarapon Koedcharoenphon', 'Male', '2', 'Staff'),
+(6936938, 'A57586', 'Akarapon Koedcharoenphon', 'Male', '0908426219', 'Admin');
 
 --
 -- Indexes for dumped tables
@@ -103,6 +165,12 @@ CREATE TABLE `staffs` (
 --
 ALTER TABLE `customers`
   ADD PRIMARY KEY (`CustomerID`);
+
+--
+-- Indexes for table `discountrate`
+--
+ALTER TABLE `discountrate`
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- Indexes for table `products`
@@ -137,31 +205,37 @@ ALTER TABLE `staffs`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `discountrate`
+--
+ALTER TABLE `discountrate`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `sales`
 --
 ALTER TABLE `sales`
-  MODIFY `SaleID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `SaleID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `sale_details`
 --
 ALTER TABLE `sale_details`
-  MODIFY `SaleDetailID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `SaleDetailID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `staffs`
 --
 ALTER TABLE `staffs`
-  MODIFY `StaffID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `StaffID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6936940;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
